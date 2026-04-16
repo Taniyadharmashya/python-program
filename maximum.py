f1593052635be72481921298b9251d10c45b7644
Taniyadharmashya <taniyadharmasya@gmail.com>
2026-04-16 22:07:15 +0530

    Add maximum.py to find the largest of three numbers

---
+++ maximum.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+max_num = a
+
+if b > max_num:
+    max_num = b
+
+if c > max_num:
+    max_num = c
+
+print("Maximum number is:", max_num)
